c3ac6ecdcebfe76b4ffeaca3358dc54c4901f975
Trevor Mearns <spiral272@gmail.com>
2021-03-16 12:57:04 +0900

    ready for code review approval

--- scripts.js
+++ scripts.js
@@ -1,103 +1,108 @@
-const date = document.getElementById("date");
-const tbody= document.querySelector("tbody");
-const selectOption=document.querySelector("select");
-const spentAmount=document.getElementById("cash-value");
-const itemDescription=document.querySelector(".item-description");
-const addButton= document.getElementById("add-button");
-const deleteButton=document.getElementById("remove-button");
+const form = document.getElementById('form');
+const date = document.getElementById('date');
+const tableBody = document.querySelector('tbody');
+const selectOption = document.querySelector('select');
+const itemLocation = document.getElementById('item-location');
+const spentAmount = document.getElementById('cash-value');
+const itemDescription = document.querySelector('.item-description');
+const addButton = document.getElementById('add-button');
+const deleteButton = document.getElementById('remove-button');
 //==========EVENT LISTENERS=======
-addButton.addEventListener("click", ()=>{
-    const dateInput=dateFixer(date.value);
-    const typeSpent=selectOption.value;
-    const numberSpent=stringToNumber(spentAmount.value);
-    const spentDetails=itemDescription.value;
-    let completedChecks=false;
-    completedChecks=inputChecker(dateInput,typeSpent,numberSpent,spentDetails);
-    if(completedChecks ===false){
-        return;
-    }else {
-    const newRow =tableBuilder(dateInput,typeSpent,numberSpent,spentDetails);
-    tbody.appendChild(newRow);
-    fieldClear()
-}})
-tbody.addEventListener("click", (e)=>{
-    seletedColorChanger(e.target);
-})
-deleteButton.addEventListener("click",()=>{
-     const trAll=document.querySelectorAll("tr");
-     trAll.forEach(removeSelectedItems)
-      
-     
+addButton.addEventListener('click', (e) => {
+  e.preventDefault();
 
-})
-//==========FUNCTIONS=============//
-function removeSelectedItems(item){
-    item.style.backgroundColor
-    if(item.style.backgroundColor ==="red"){
-    tbody.removeChild(item);
-    }
-}
-//fixed returned date from input filed from yyyy/mm/dd to mm/dd/yyyy
-function dateFixer(offDate){
-   if (offDate==""){
-       return ;
-   }
-    let year =offDate.slice(5);
-    year+="-"+offDate.slice(0,4);
-    return year
-}
+  const dateInput = date.value;
+  const paymentType = selectOption.value;
+  const itemAmount = spentAmount.value;
+  const purchasedAt = itemLocation.value;
+  const itemDetails = itemDescription.value;
+  let completedChecks = false;
+  completedChecks = inputChecker(
+    dateInput,
+    paymentType,
+    itemAmount,
+    purchasedAt,
+    itemDetails
+  );
+  if (completedChecks === false) {
+    return;
+  } else {
+    const newRow = tableBuilder(
+      dateInput,
+      paymentType,
+      itemAmount,
+      purchasedAt,
+      itemDetails
+    );
+    tableBody.appendChild(newRow);
+    form.reset();
+  }
+});
+tableBody.addEventListener('click', (e) => {
+  seletedColorChanger(e.target);
+});
+
 //build complete table once check have bee maid
-function tableBuilder(date,type,amount,desc){
+function tableBuilder(date, paymentType, amount, purchasedAt, itemDescription) {
+  let tableRow = document.createElement('tr');
 
-    let tableRow=document.createElement("tr");
-    tableRow.innerHTML=
-    `<td>${date}</td>
-    <td>${type}</td>
-    <td>${amount}</td>
-    <td>${desc}</td>`
-    return tableRow;
-    
+  const dateCell = document.createElement('td');
+  dateCell.textContent = `${date}`;
+  tableRow.appendChild(dateCell);
+
+  const paymentTypeCell = document.createElement('td');
+  paymentTypeCell.textContent = `${paymentType}`;
+  tableRow.appendChild(paymentTypeCell);
+
+  const amountCell = document.createElement('td');
+  amountCell.textContent = `$${amount}`;
+  tableRow.appendChild(amountCell);
+
+  const purchasedAtCell = document.createElement('td');
+  purchasedAtCell.textContent = `${purchasedAt}`;
+  tableRow.appendChild(purchasedAtCell);
+
+  const itemDescriptionCell = document.createElement('td');
+  itemDescriptionCell.textContent = `${itemDescription}`;
+  tableRow.appendChild(itemDescriptionCell);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.setAttribute('id', 'delete-button');
+  tableRow.appendChild(deleteButton);
+
+  deleteButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    deleteButton.parentElement.remove();
+  });
+
+  return tableRow;
 }
-//convert user number input to string
-function stringToNumber(numString){
-    let newNum=parseFloat(numString);
-    newNum=newNum.toFixed(2);
-    return newNum;
-}
-//checks every input field for empty fields 
-function inputChecker(dateInput,typeSpent,numberSpent,spentDetails){
-if(dateInput===undefined){
-    alert("please pick a date");
+
+//checks every input field for empty fields
+function inputChecker(dateInput, typeSpent, numberSpent, spentDetails) {
+  if (dateInput === '') {
+    alert('please pick a date');
     return false;
-}
-else if(typeSpent===""){
-    alert("please pick a type of currency");
+  }
+  if (typeSpent === '') {
+    alert('please pick a type of currency');
     return false;
+  }
+  if (numberSpent <= 0 || numberSpent === NaN) {
+    alert('please enter amount over $0');
+    return false;
+  }
+  if (spentDetails === '') {
+    alert('please enter spent desc');
+    return false;
+  }
+  return true;
 }
-else if(numberSpent<=0 || numberSpent=="NaN"){
-    alert("please enter amount over $0");
-return false;
+
+function seletedColorChanger(option) {
+  if (option.tagName === 'TD') {
+    const selectRow = option.parentNode;
+    selectRow.classList.toggle('tableRowBackGround');
+  }
 }
-else if (spentDetails===""){
-     alert("please enter spent desc");
-     return false;
-}
-return true;
-}
-//clear all input field after data taken
-function fieldClear(){
-    date.value="";
-    selectOption.value="";
-    spentAmount.value="";
-    itemDescription.value="";}
- function seletedColorChanger(option){   
-    if( option.tagName==="TD"){
-            const selectRow=option.parentNode;
-            if(selectRow.style.backgroundColor==="red"){
-                selectRow.style.backgroundColor="white";
-                
-            }
-            else{selectRow.style.backgroundColor="red"}
-            }
-        }
-       
